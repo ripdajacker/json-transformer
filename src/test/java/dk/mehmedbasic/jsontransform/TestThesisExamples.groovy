131b7b0981b351f09c8eb33e45702ffd899769b0
Jesenko Mehmedbasic <jekm@r-m.com>
2016-01-04 21:31:30 +0100

    Examples for thesis updated

--- src/test/java/dk/mehmedbasic/jsontransform/TestThesisExamples.groovy
+++ src/test/java/dk/mehmedbasic/jsontransform/TestThesisExamples.groovy
@@ -25,6 +25,8 @@ class TestThesisExamples {
 
     @Test
     void addValue() {
+        println("Add value")
+
         document.transform("person")
                 .add("age", 60d)
                 .apply()
@@ -32,6 +34,8 @@ class TestThesisExamples {
 
     @Test
     void addObject() {
+        println("Add object")
+
         document.transform("person")
                 .add("microphone", JsonType.Object)
                 .apply()
@@ -41,11 +45,51 @@ class TestThesisExamples {
                 .apply()
     }
 
+    @Test
+    void renameChild() {
+        println("Rename child")
+
+        document.transform("person")
+                .renameChild("name", "navn")
+                .apply()
+    }
+
+    @Test
+    void renameField() {
+        println("Rename field")
+
+        document.transform("person name")
+                .renameTo("navn")
+                .apply()
+    }
+
+    @Test
+    void deleteField() {
+        println("Delete field")
+        document.transform("person")
+                .deleteChild("name")
+                .apply()
+    }
+
+    @Test
+    void partition() {
+        println("Partition object")
+
+        document.transform("person")
+                .partition([["value1", "name"], ["value2", "occupation"]])
+                .apply()
+
+        document.transform("")
+                .deleteChild("person")
+                .apply()
+    }
+
     @After
     void printOut() {
         def node = JacksonConverter.asJacksonNode(document)
         def string = new ObjectMapper().writer().writeValueAsString(node)
         println(string)
+        println()
     }
 
 }
