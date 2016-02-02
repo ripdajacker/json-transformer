2a5be101fd796e1812c8687b901385d5051e0ef4
Jesenko Mehmedbasic <jekm@r-m.com>
2016-02-02 13:40:19 +0100

    Naming conversion strategies to support the thesis examples

---
+++ src/main/java/dk/mehmedbasic/jsonast/conversion/InlineIdsNamingStrategy.groovy
@@ -0,0 +1,56 @@
+package dk.mehmedbasic.jsonast.conversion
+
+import dk.mehmedbasic.jsonast.BaseNode
+import dk.mehmedbasic.jsonast.JsonIdentifier
+import groovy.transform.TypeChecked
+import org.codehaus.jackson.node.ArrayNode
+import org.codehaus.jackson.node.ObjectNode
+import org.codehaus.jackson.node.ValueNode
+
+import java.util.regex.Pattern
+
+/**
+ * Naming strategy for inline id
+ */
+@TypeChecked
+class InlineIdsNamingStrategy implements ConversionNamingStrategy {
+    private static Pattern PATTERN_NAME_AND_ID = Pattern.compile(/([A-Za-z0-9]+) #(\-?[0-9a-f]+)\s*$/)
+
+    @Override
+    String toJackson(BaseNode node) {
+        if (node.identifier.id != null) {
+            return "${node.identifier.name} #${node.identifier.id}"
+        }
+        return node.identifier.name
+    }
+
+    @Override
+    JsonIdentifier toTransformable(String name, ObjectNode objectNode) {
+        return parseName(name)
+    }
+
+    @Override
+    JsonIdentifier toTransformable(String name, ArrayNode arrayNode) {
+        return parseName(name)
+    }
+
+    @Override
+    JsonIdentifier toTransformable(String name, ValueNode valueNode) {
+        return parseName(name)
+    }
+
+    private static JsonIdentifier parseName(String name) {
+        def identifier = new JsonIdentifier(name)
+        if (name == null) {
+            return identifier
+        }
+
+        def matcher = PATTERN_NAME_AND_ID.matcher(name)
+        if (matcher.find()) {
+            identifier.name = matcher.group(1)
+            identifier.id = matcher.group(2)
+        }
+
+        return identifier
+    }
+}
--- src/main/java/dk/mehmedbasic/jsonast/conversion/JacksonConverter.groovy
+++ src/main/java/dk/mehmedbasic/jsonast/conversion/JacksonConverter.groovy
@@ -1,119 +1,155 @@
 package dk.mehmedbasic.jsonast.conversion
 
 import dk.mehmedbasic.jsonast.*
+import groovy.transform.TypeChecked
 import org.codehaus.jackson.JsonNode
 import org.codehaus.jackson.node.*
 
 /**
  * Converts jackson nodes to dk.mehmedbasic.jsonast nodes.
  */
+@TypeChecked
 class JacksonConverter {
+
     /**
      * Converts a Jackson node to a JsonDocument.
      *
      * @param root the root to convert.
+     * @param namingStrategy the naming strategy.
      *
      * @return the converted document.
      */
-    static JsonDocument asTransformable(JsonNode root) {
-        def document = new JsonDocument()
-        document.addRoot(convertToTransformable(null, root))
-        return document
-    }
-
-    static JsonNode asJacksonNode(JsonDocument document) {
-        return convertToJackson(document.roots.get(0))
-    }
-
-    private static JsonNode convertToJackson(BaseNode baseNode) {
-        if (baseNode.isArray()) {
-            def result = new ArrayNode(JsonNodeFactory.instance)
-            JsonArrayNode arrayNode = baseNode as JsonArrayNode
-            for (BaseNode childNode : arrayNode.children) {
-                result.add(convertToJackson(childNode))
-            }
-            return result
-        }
-        if (baseNode.isValueNode()) {
-            JsonValueNode valueNode = baseNode as JsonValueNode
-            if (baseNode.isInt()) {
-                return new IntNode(valueNode.intValue())
-            } else if (baseNode.isDouble()) {
-                return new DoubleNode(valueNode.doubleValue())
-            } else if (baseNode.isBoolean()) {
-                if (valueNode.booleanValue()) {
-                    return BooleanNode.TRUE
-                } else {
-                    return BooleanNode.FALSE
-                }
-            } else if (baseNode.isString()) {
-                return new TextNode(valueNode.stringValue())
-            }
-        }
-        if (baseNode.isObject()) {
-            def result = new ObjectNode(JsonNodeFactory.instance)
-            def objectNode = baseNode as JsonObjectNode
-
-            for (BaseNode childNode : objectNode.children) {
-                def identifier = childNode.identifier
-                result.put(identifier.name, convertToJackson(childNode))
-            }
-            return result
-        }
-        throw new IllegalArgumentException("Unknown node type: $baseNode")
+    static JsonDocument asTransformable(JsonNode root, ConversionNamingStrategy namingStrategy = null) {
+        return new Converter(namingStrategy).asTransformable(root)
     }
 
     /**
-     * Converts the given node.
+     * Converts the transformable into a Jackson tree.
      *
-     * @param name the name of the node.
-     * @param source the source Jackson node.
+     * @param document the document
+     * @param namingStrategy the naming strategy.
      *
-     * @return the converted node.
+     * @return the resulting node.
      */
-    private static BaseNode convertToTransformable(String name, JsonNode source) {
-        if (source.isObject()) {
-            def result = new JsonObjectNode()
-            result.identifier.name = name
-            result.identifier.classes.add("object")
+    static JsonNode asJacksonNode(JsonDocument document, ConversionNamingStrategy namingStrategy = null) {
+        return new Converter(namingStrategy).asJacksonNode(document)
+    }
 
-            ObjectNode object = source as ObjectNode
-            for (Map.Entry<String, JsonNode> entry : object.fields) {
-                result.addChild(convertToTransformable(entry.key, entry.value))
-            }
-            return result
-        } else if (source.isArray()) {
-            def result = new JsonArrayNode()
-            result.identifier.name = name
-            result.identifier.classes.add("array")
 
-            ArrayNode array = source as ArrayNode
-            for (JsonNode node : array) {
-                result.addChild(convertToTransformable(null, node))
-            }
-            return result
-        } else if (source.isValueNode()) {
-            def result = new JsonValueNode()
-            result.identifier.name = name
-            if (name == "@version") {
-                result.identifier.classes << "sysclass_version"
-            }
+    private static final class Converter {
+        ConversionNamingStrategy strategy
 
-            if (source.isBoolean()) {
-                result.identifier.classes.add("boolean")
-                result.setValue(source.booleanValue)
-            } else if (source.isInt()) {
-                result.identifier.classes.add("int")
-                result.setValue(source.intValue)
-            } else if (source.isTextual()) {
-                result.identifier.classes.add("string")
-                result.setValue(source.textValue)
-            } else if (source.isDouble()) {
-                result.identifier.classes.add("double")
-                result.setValue(source.doubleValue)
+        Converter(ConversionNamingStrategy strategy) {
+            this.strategy = strategy
+            if (strategy == null) {
+                this.strategy = new DefaultNamingStrategy()
             }
-            return result
         }
-        return null
+
+        JsonDocument asTransformable(JsonNode root) {
+            def document = new JsonDocument()
+            document.addRoot(convertToTransformable(null, root))
+            return document
+        }
+
+        JsonNode asJacksonNode(JsonDocument document) {
+            return convertToJackson(document.iterator().next())
+        }
+
+
+        private JsonNode convertToJackson(BaseNode baseNode) {
+            if (baseNode.isArray()) {
+                def result = new ArrayNode(JsonNodeFactory.instance)
+                JsonArrayNode arrayNode = baseNode as JsonArrayNode
+                for (BaseNode childNode : arrayNode.children) {
+                    result.add(convertToJackson(childNode))
+                }
+                return result
+            }
+            if (baseNode.isValueNode()) {
+                JsonValueNode valueNode = baseNode as JsonValueNode
+                if (baseNode.isInt()) {
+                    return new IntNode(valueNode.intValue())
+                } else if (baseNode.isDouble()) {
+                    return new DoubleNode(valueNode.doubleValue())
+                } else if (baseNode.isBoolean()) {
+                    if (valueNode.booleanValue()) {
+                        return BooleanNode.TRUE
+                    } else {
+                        return BooleanNode.FALSE
+                    }
+                } else if (baseNode.isString()) {
+                    return new TextNode(valueNode.stringValue())
+                }
+            }
+            if (baseNode.isObject()) {
+                def result = new ObjectNode(JsonNodeFactory.instance)
+                def objectNode = baseNode as JsonObjectNode
+
+                for (BaseNode childNode : objectNode.children) {
+                    result.put(strategy.toJackson(childNode), convertToJackson(childNode))
+                }
+                return result
+            }
+            throw new IllegalArgumentException("Unknown node type: $baseNode")
+        }
+
+        /**
+         * Converts the given node.
+         *
+         * @param name the name of the node.
+         * @param source the source Jackson node.
+         *
+         * @return the converted node.
+         */
+        private BaseNode convertToTransformable(String name, JsonNode source) {
+            if (source.isObject()) {
+                def result = new JsonObjectNode()
+                result.identifier = strategy.toTransformable(name, source as ObjectNode)
+                result.identifier.classes.add("object")
+
+                ObjectNode object = source as ObjectNode
+                Iterator<Map.Entry<String, JsonNode>> fields = object.fields
+                while (fields.hasNext()) {
+                    Map.Entry<String, JsonNode> entry = fields.next();
+                    result.addChildNoUpdate(convertToTransformable(entry.key, entry.value))
+                }
+                result.updateMap()
+
+                return result
+            } else if (source.isArray()) {
+                def result = new JsonArrayNode()
+                result.identifier = strategy.toTransformable(name, source as ArrayNode)
+                result.identifier.classes.add("array")
+
+                ArrayNode array = source as ArrayNode
+                for (JsonNode node : array) {
+                    result.addChild(convertToTransformable(null, node))
+                }
+                return result
+            } else if (source.isValueNode()) {
+                def result = new JsonValueNode()
+                result.identifier = strategy.toTransformable(name, source as ValueNode)
+                if (name == "@version") {
+                    result.identifier.classes << "sysclass_version"
+                }
+
+                if (source.isBoolean()) {
+                    result.identifier.classes.add("boolean")
+                    result.setValue(source.booleanValue)
+                } else if (source.isInt()) {
+                    result.identifier.classes.add("int")
+                    result.setValue(source.intValue)
+                } else if (source.isTextual()) {
+                    result.identifier.classes.add("string")
+                    result.setValue(source.textValue)
+                } else if (source.isDouble()) {
+                    result.identifier.classes.add("double")
+                    result.setValue(source.doubleValue)
+                }
+                return result
+            }
+            return null
+        }
     }
 }
